edd19b55b3a9dee270eb853023d69b181ddb7329
Jacob Beu <jacobbeu@gmail.com>
2024-12-11 18:34:23 -0600

    part 2 works, but takes 30seconds :(

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -6,7 +6,7 @@ pub fn solve() -> SolutionPair {
     let contents = include_str!("../../input/day07_input.txt");
 
     let solution1 = calibrate(&contents);
-    let solution2 = 0;
+    let solution2 = calibrate_2(&contents);
 
     (Solution::from(solution1), Solution::from(solution2))
 }
@@ -96,6 +96,105 @@ fn calibrate_line(data: &Vec<usize>) -> usize {
     0
 }
 
+
+fn calibrate_2(data: &str) -> usize {
+
+    let calibration_tests: Vec<Vec<usize>> = 
+        data.lines()
+        .map(|line| parse_line(line))
+        .collect();
+
+    let result: usize = calibration_tests.iter()
+        .map(|line| calibrate_line_2(line))
+        .sum();
+
+    result
+}
+
+fn calibrate_line_2(data: &Vec<usize>) -> usize {
+
+    if data.len() < 2 {
+        return 0;
+    }
+
+    let result = data[0];
+
+    if data.len() == 2 {
+        if data[0] == data[1] {
+            return data[0];
+        }
+        else {
+            return 0;
+        }
+    }
+
+    let mut operators = vec![0; data.len() - 2];
+
+    loop {
+        let mut operator_counter = 1;
+        let mut data_counter = 3;
+
+        let mut product = 0;
+
+        // init product
+        if operators[0] == 0 {
+            product += data[1] + data[2];
+        } 
+        else if operators[0] == 1 {
+            product += data[1] * data[2];
+        }
+        else {
+            let concat = format!("{}{}", data[1].to_string(), data[2].to_string());
+            product += concat.parse::<usize>().expect("Failed to parse String");
+        }
+
+        // loop the remaining operators
+        while operator_counter < operators.len() {
+
+            let prev_value = product;
+            if operators[operator_counter] == 0 {
+                product += data[data_counter];
+            }
+            else if operators[operator_counter] == 1 {
+                product *= data[data_counter];
+            }
+            else {
+                let concat = format!("{}{}", product.to_string(), data[data_counter].to_string());
+                product = concat.parse::<usize>().expect("Failed to parse String");
+            }
+
+            operator_counter += 1;
+            data_counter += 1;
+        }
+
+        if product == result {
+            return result;
+        }
+
+        if operators.iter().all(|&item| item == 2) {
+            break;
+        }
+
+        let mut operator_idx = 0;
+        loop {
+            if operators[operator_idx] == 0 {
+                operators[operator_idx] = 1;
+                break;
+            }
+            else if operators[operator_idx] == 1 {
+                operators[operator_idx] = 2;
+                break;
+            }
+            else {
+                operators[operator_idx] = 0;
+                operator_idx += 1;
+            }
+        }
+    }
+
+    0
+}
+
 fn parse_line(data: &str) -> Vec<usize> {
 
     let mut segments = data.split(" ");
@@ -138,11 +237,13 @@ mod tests {
     }
 
     #[test]
-    fn day06_example() {
+    fn example() {
         let example = include_str!("../../samples/day07_sample.txt");
 
         let result = calibrate(&example);
+        let result2 = calibrate_2(&example);
 
         assert_eq!(result, 3749);
+        assert_eq!(result2, 11387);
     }
 }
